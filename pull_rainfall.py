551e94990e96cd1898a7d79731646bd38b8a054d
Owen Varley <o.w.varley@gmail.com>
2020-10-29 22:03:17 +0000

    Display the date and reading for all values in the API

--- pull_rainfall.py
+++ pull_rainfall.py
@@ -1,5 +1,7 @@
 import requests
+import json
 
 response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/stations/E24712/readings.json?today&_sorted&parameter=rainfall")
 
-print (response.status_code)
+for item in response.json()["items"]:
+    print (item["dateTime"] + " " + str(item["value"]))
