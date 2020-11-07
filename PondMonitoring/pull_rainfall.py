c335252eb1f281850c12239783b1e478502b32ba
Owen Varley <o.w.varley@gmail.com>
2020-11-07 15:04:30 +0000

    Save pond level readings to a daily csv file

--- PondMonitoring/pull_rainfall.py
+++ PondMonitoring/pull_rainfall.py
@@ -1,7 +1,32 @@
 import requests
 import json
+import os
+import time
 
-response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/stations/E24712/readings.json?today&_sorted&parameter=rainfall")
+PATH_DATA = str(os.environ['PATH_DATA'])
+DATA_FILE = "rainfall_data"
+DATA_FILE_TYPE = ".csv"
+RAINFALL_API_CALL = "https://environment.data.gov.uk/flood-monitoring/id/stations/E24712/readings.json?today&_sorted&parameter=rainfall"
+
+class RainfallReading:
+    def __init__ (self, time, reading):
+        self.time = time
+        self.reading = reading
+
+    def __str__ (self):
+        return self.time + ", " + str(self.reading)
+
+response = requests.get(RAINFALL_API_CALL)
+
+readings = []
 
 for item in response.json()["items"]:
-    print (item["dateTime"] + " " + str(item["value"]))
+    readings.append(RainfallReading(item["dateTime"], item["value"]))
+
+filename = PATH_DATA + DATA_FILE + time.strftime("_%Y%m%d") + DATA_FILE_TYPE
+
+with open(filename, "w") as f:
+    for reading in readings:
+        f.write(str(reading) + "\n")
+
+print ("Total readings: " + str(len(readings)))
