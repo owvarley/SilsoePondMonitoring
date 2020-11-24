bd6f4310c38aba356d4f6323de92430199664530
Owen Varley <o.w.varley@gmail.com>
2020-11-24 21:27:42 +0000

    Add status output

--- PondMonitoring/main.py
+++ PondMonitoring/main.py
@@ -12,11 +12,15 @@ def main(curr_hour, hour_to_capture, sleep_24, sleep_1):
         if curr_hour >= hour_to_capture:
             # Pull and save the rainfall data once a day after a given hour to ensure
             # a full set of data is retained
+            print (str(datetime.datetime.now()) + " capturing data.")
             pull_rainfall.Rainfall.Download()
+            print ("Waiting...")
             time.sleep(sleep_24)
         else:
             time.sleep(sleep_1)
 
 if __name__ == "__main__":
+    print ("Starting Pond Monitor...")
+    print ("Hour to Capture: " + str(HOUR_TO_CAPTURE))
     now = datetime.datetime.now()
     main(now.hour, HOUR_TO_CAPTURE, SLEEP_24_HOUR, SLEEP_1_HOUR)
