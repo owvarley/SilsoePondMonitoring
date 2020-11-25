3cbae00154a43c5b11b018da7c991aa40eb81e7c
Owen Varley <o.w.varley@gmail.com>
2020-11-25 21:58:05 +0000

    Changed API end point to use CSV for previous day

--- PondMonitoring/pull_rainfall.py
+++ PondMonitoring/pull_rainfall.py
@@ -1,34 +1,25 @@
 import requests
-import json
 import os
-import time
+import datetime
 
 PATH_DATA = str(os.environ['PATH_DATA'])
 DATA_FILE = "rainfall_data"
 DATA_FILE_TYPE = ".csv"
-RAINFALL_API_CALL = "https://environment.data.gov.uk/flood-monitoring/id/stations/E24712/readings.json?today&_sorted&parameter=rainfall"
-
-class RainfallReading:
-    def __init__ (self, time, reading):
-        self.time = time
-        self.reading = reading
-
-    def __str__ (self):
-        return self.time + ", " + str(self.reading)
+RAINFALL_API_ENDPOINT = "https://environment.data.gov.uk/flood-monitoring/id/stations/E24712/readings.csv?parameter=rainfall&_sorted&date="
+ONE_DAY = datetime.timedelta(days=1)
 
 class Rainfall:
     @staticmethod
     def Download():
-        response = requests.get(RAINFALL_API_CALL)
-        readings = []
+        now = datetime.datetime.now()
+        yesterday = now - ONE_DAY
 
-        for item in response.json()["items"]:
-            readings.append(RainfallReading(item["dateTime"], item["value"]))
+        yesterday_str = yesterday.strftime("%Y-%m-%d")
+        filename_str = ''.join([PATH_DATA, DATA_FILE, "_", yesterday.strftime("%Y%m%d_%H%M%S"), DATA_FILE_TYPE])
 
-        filename = PATH_DATA + DATA_FILE + time.strftime("_%Y%m%d_%H%M%S") + DATA_FILE_TYPE
+        response = requests.get(RAINFALL_API_ENDPOINT + yesterday_str)
 
-        with open(filename, "w") as f:
-            for reading in readings:
-                f.write(str(reading) + "\n")
+        with open(filename_str, "w") as f:
+            f.write(response.text)
 
-        print ("Total readings: " + str(len(readings)))
+        print ("Total bytes written: " + str(len(response.content)))
