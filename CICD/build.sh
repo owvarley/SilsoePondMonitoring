ee54f7a71628cfb61f0f1868bae292caad947928
owvarley <owen@node-tech.uk>
2020-12-24 22:07:32 +0000

    Add script to build and push Dockerfiles for projects

---
+++ CICD/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# $1 == The Project Path to build
+
+# Get the version for the project to use for the tag
+ProjectPath=$1
+ProjectVersion=$(cat $ProjectPath/VERSION)
+ProjectPathLower=$(echo $ProjectPath | tr '[:upper:]' '[:lower:]')
+echo "docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t owenvarley/silsoe$ProjectPathLower:$ProjectVersion --push ."
